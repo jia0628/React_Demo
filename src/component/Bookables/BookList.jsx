f25af6f9a6acbc9bd95df3a116d87e77618ba8df
jia0628 <034300sss@naver.com>
2024-10-01 17:44:44 +0900

    #3 useState (상태값 관리) bookableIndex 값이 바뀔때마다 리액트가 UI 를 다시 렌더링 하도록 합니다. Next 버튼 이벤트도 상태값 변경 베소드를 실행하도록 합니다.

--- src/component/Bookables/BookList.jsx
+++ src/component/Bookables/BookList.jsx
@@ -1,24 +1,42 @@
 import { bookables } from "../../static.json"
+import {useState} from "react";
+import {FaArrowRight} from "react-icons/fa";
 
 function BookList() {
     const group = "Rooms"
-    let bookableIndex = 0
+    // let bookableIndex = 0
     const bookableGroup = bookables.filter(b => (b.group === group))
 
-    function changeBookable(selectIndex) {
-        bookableIndex = selectIndex
+    // ★ bookableIndex 는 상태값 관리를 해야할 변수
+    // ★ setBookableIndex 는 useState 가 리턴해주는 메소드. (== 값 변경 메소드)
+    const [bookableIndex, setBookableIndex] = useState(0)  // ★ 이부분이 핵심, useState 는 2개의 리턴값을 가짐(bookableIndex, setBookableIndex)
+
+    // function changeBookable(selectIndex) {
+    //     bookableIndex = selectIndex
+    // }
+
+    function nextBookableIndex() {
+        // 상태값 변경 메소드의 인자 i는 bookableIndex 값
+        setBookableIndex((i) => (i+1) % bookableGroup.length)
     }
 
     return(
-        <ul className="items-list-nav">
-            {bookableGroup.map((b, i) => (
-                <li key={b.id} className={i === bookableIndex? "selected":null}>
-                    <button className="btn" onClick={() => {changeBookable(i)}}>
-                        {b.title}
-                    </button>
-                </li>
-            ))}
-        </ul>
+        <>
+            <ul className="items-list-nav">
+                {bookableGroup.map((b, i) => (
+                    <li key={b.id} className={i === bookableIndex? "selected":null}>
+                        <button className="btn" onClick={() => {setBookableIndex(i)}}>
+                            {b.title}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+            <p>
+                <button className="btn" onClick={nextBookableIndex}>
+                    <FaArrowRight/><span>Next</span>
+                </button>
+            </p>
+        </>
     )
 }
 
