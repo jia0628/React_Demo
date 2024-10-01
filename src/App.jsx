37ca18ef03ce490b62f121e7c417ccf4608420b9
jia0628 <034300sss@naver.com>
2024-10-01 16:15:27 +0900

    #3 예약 프로젝트 nav Link, BrowserRouter 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 // 초기 App.jsx
 import './App.css'
-import Garage, {Car} from "./basic/List.jsx";
-import {GarageKorean} from "./basic/Objects.jsx";  // ★ 잘 확인 !! default 로 내보내기하면 {}가 없음 (List.jsx 참고)
+import {BrowserRouter, Link} from "react-router-dom";
+import {FaCalendarAlt, FaDoorOpen, FaUsers} from "react-icons/fa";
+import UserPicker from "./component/Users/UserPicker.jsx";
 
 // index.html 이 처음 요청으로 반환되는 페이지
 //       html 은 더 이상 만들지 않습니다.  :  SPA
@@ -9,12 +10,36 @@ import {GarageKorean} from "./basic/Objects.jsx";  // ★ 잘 확인 !! default 
 function App() {
 
   return (
-    <>
-        {/* brand 라는 임의 프로퍼티 설정 */}
-        <Car brand="소나타"/>
-        <Garage/>
-        <GarageKorean/>
-    </>
+      <BrowserRouter>
+        <div className={App}>
+          <header>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/bookings" className="btn btn-header">
+                    <FaCalendarAlt/>
+                    <span>예약 하기</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/bookables" className="btn btn-header">
+                    <FaDoorOpen/>
+                    <span>예약 자원</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/users" className="btn btn-header">
+                    <FaUsers/>
+                    <span>사용자</span>
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+            <UserPicker/>
+          </header>
+        </div>
+
+      </BrowserRouter>
   )
 }
 
---
+++ src/component/Bookables/BookablePage.jsx
@@ -0,0 +1,8 @@
+function BookablePage() {
+    return(
+        <main className="bookables-page">
+            <h4>예약 가능 현황</h4>
+        </main>
+    )
+}
+export default BookablePage;
